d2ae23a72687c8057734c7f09c5b08fd5c34d239
ArthurPons <pons.arthur@gmail.com>
2015-05-06 14:42:16 +0200

    Ajout de projet.pl

---
+++ projet.pl
@@ -0,0 +1,25 @@
+faussesMarchandises([[maïs, riz, ble, ble],
+[ble, maïs, sucre, riz],
+[cafe, sucre, cacao, riz],
+[sucre, maïs],
+[cacao, maïs, ble, sucre],
+[],
+[cafe, ble, sucre, cacao],
+[maïs, cacao,ble],
+[riz,riz,cafe,cacao]]).
+
+fausseBourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
+
+afficheMarchandise([]).
+afficheMarchandise([[]|Q]):-!, afficheMarchandise(Q).
+afficheMarchandise([[T|L]|Q]):-write(T), write('\t'),afficheMarchandise(Q). 
+
+afficheQuantite([]).
+afficheQuantite([[]|Q]):-!, afficheQuantite(Q).
+afficheQuantite([T|Q]):-length(T,N), write(N), write('\t'),afficheQuantite(Q). 
+
+afficheTrader(1):-write('x'),!.
+afficheTrader(N):-write('\t'),M is N-1, afficheTrader(M).
+
+afficheBourse([]).
+afficheBourse([[T|L]|Q]):-write(T),write(':\t'),write(L),nl,afficheBourse(Q).
