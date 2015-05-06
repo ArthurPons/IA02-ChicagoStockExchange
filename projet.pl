d76f37e249e5ed09cec4262eabcebe1c51c1c202
ArthurPons <pons.arthur@gmail.com>
2015-05-06 15:16:22 +0200

    Ajout saut de ligne affichage

--- projet.pl
+++ projet.pl
@@ -11,11 +11,11 @@ faussesMarchandises([[maïs, riz, ble, ble],
 fausseBourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
 
 afficheMarchandise([]).
-afficheMarchandise([[]|Q]):-!, afficheMarchandise(Q).
+afficheMarchandise([[]|Q]):-!, afficheMarchandise(Q),nl.
 afficheMarchandise([[T|L]|Q]):-write(T), write('\t'),afficheMarchandise(Q). 
 
 afficheQuantite([]).
-afficheQuantite([[]|Q]):-!, afficheQuantite(Q).
+afficheQuantite([[]|Q]):-!, afficheQuantite(Q),nl.
 afficheQuantite([T|Q]):-length(T,N), write(N), write('\t'),afficheQuantite(Q). 
 
 afficheTrader(1):-write('x'),!.
